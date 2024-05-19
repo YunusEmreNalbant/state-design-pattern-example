8bcd886edf6645e2a5aa695d987d1ad6e723da9c
Yunus Emre Nalbant <yunus.nalbant0@gmail.com>
2024-05-19 16:44:00 +0300

    add example

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+
+interface IOrderState {
+    public function next($order);
+    public function previous($order);
+    public function getCurrentStatus();
+}
+
+class DeliveredState implements IOrderState {
+    public function next($order) {
+        echo "Bu durumun sonrası yoktur. Bu son durumdur. ❌\n";
+    }
+
+    public function previous($order) {
+        $order->setState(new OnTheWayState());
+    }
+
+    public function getCurrentStatus() {
+        echo "Sipariş Teslim Edildi 🚚\n";
+    }
+}
+
+class OnTheWayState implements IOrderState {
+    public function next($order) {
+        $order->setState(new DeliveredState());
+    }
+
+    public function previous($order) {
+        $order->setState(new ProcessingState());
+    }
+
+    public function getCurrentStatus() {
+        echo "Sipariş Yolda 🛣️\n";
+    }
+}
+
+class ProcessingState implements IOrderState {
+    public function next($order) {
+        $order->setState(new OnTheWayState());
+    }
+
+    public function previous($order) {
+        $order->setState(new NewOrderState());
+    }
+
+    public function getCurrentStatus() {
+        echo "Sipariş İşleme Alınıyor 🏭\n";
+    }
+}
+
+class NewOrderState implements IOrderState {
+    public function next($order) {
+        $order->setState(new ProcessingState());
+    }
+
+    public function previous($order) {
+        echo "Bu durumun öncesi yoktur. Bu ilk durumdur. ❌\n";
+    }
+
+    public function getCurrentStatus() {
+        echo "Sipariş Verildi 📝\n";
+    }
+}
+
+class Order {
+    private $state;
+
+    public function __construct() {
+        $this->state = new NewOrderState();
+    }
+
+    public function setState($state) {
+        $this->state = $state;
+    }
+
+    public function nextState() {
+        $this->state->next($this);
+    }
+
+    public function previousState() {
+        $this->state->previous($this);
+    }
+
+    public function getOrderState() {
+        $this->state->getCurrentStatus();
+    }
+}
+
+$order = new Order(); // Sipariş Oluşturuluyor...
+$order->getOrderState(); // Sipariş Verildi 📝
+$order->nextState();
+$order->getOrderState(); // Sipariş İşleme Alınıyor 🏭
+$order->nextState();
+$order->getOrderState(); // Sipariş Yolda 🛣️
+$order->nextState();
+$order->getOrderState(); // Sipariş Teslim Edildi 🚚
+$order->nextState(); // Bu durumun sonrası yoktur. Bu son durumdur. ❌
